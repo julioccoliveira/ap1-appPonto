a735a2122d3f7bd4d1c46f53282f6f63a71b4b4b
Julioccdoliveira <juliocc_oliveira@hotmail.com>
2020-11-29 17:13:18 -0300

    Insert adicionado

--- app/src/main/java/br/com/julio/ap1_appponto/ChecklistDAO.java
+++ app/src/main/java/br/com/julio/ap1_appponto/ChecklistDAO.java
@@ -1,9 +1,43 @@
 package br.com.julio.ap1_appponto;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.List;
+
 public class ChecklistDAO {
-    public void insert(Checklist  chk){   }
-    public void remove(int id){   }
-    public void alter(Checklist chk){   }
-    public List<Checklist> listChecklists(){ return null;  }
-    public Checklist getChecklist(int id){return null;}
+
+    SQLiteDatabase conexao;
+
+    public ChecklistDAO(SQLiteDatabase conexao) {
+        this.conexao = conexao;
+
     }
+
+    public void insert(Checklist chk){
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Matricula", chk.getMatricula());
+        contentValues.put("Latitude",chk.getLatitude());
+        contentValues.put("Longitude", chk.getLongitude());
+        contentValues.put("Data e hora", chk.getTimeStamp());
+        conexao.insertOrThrow("CHecklist",null, contentValues);
+
+    }
+    public void remove(int id){
+
+    }
+    public void alter(Checklist chk){
+
+    }
+    public List<Checklist> listChecklists(){
+
+        return null;
+
+    }
+    public Checklist getChecklist(int id){
+
+        return null;
+    }
+
+}
